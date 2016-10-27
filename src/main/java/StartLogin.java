ed8b8f52ccea333b90d370ee34566d465cc7aec6
VitekRomanov <gizinax@yandex.ru>
2016-10-27 17:12:45 +0300

    START

---
+++ src/main/java/StartLogin.java
@@ -0,0 +1,18 @@
+package main.java;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StartLogin {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:\\Workspace\\drivers\\chromedriver.exe"); //путь к твоему chromedriver.exe
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://mail.yandex.by");
+        
+
+        YandexMail yandexmail = new YandexMail(driver);
+        yandexmail.loginTo("qwe", "nuasdll");
+    }
+}     
---
+++ src/main/java/YandexMail.java
@@ -0,0 +1,30 @@
+package main.java;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+
+public class YandexMail {
+	
+	WebDriver driver;
+
+	
+	@FindBy(name = "login")
+	private  WebElement login;
+	
+	@FindBy(name = "passwd")
+	private  WebElement passwd;
+	
+	YandexMail(WebDriver driver){
+		this.driver =driver;
+	}
+	
+	public void loginTo (String username,String userpass ){
+		login.sendKeys(username);
+		passwd.sendKeys(userpass);
+		passwd.submit();
+	}
+
+}
